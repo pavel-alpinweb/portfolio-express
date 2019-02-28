0a98ebaa6d5d6f90f1d2bb58501ddf7a4fe8709e
Pavel Bezdornov <pavel.alpinweb@yandex.ru>
2019-02-28 09:30:55 +0600

    create server

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 var express = require("express");
 var app = express();
+
+app.get("/", function(req, res) {
+  res.send("Hello Express!");
+});
+
+app.listen(8000, function() {
+  console.log("Портфолио на express запущенно на порту 8000!");
+});
