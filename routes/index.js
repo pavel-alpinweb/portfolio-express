bfd3dc327ea46724c2cfa5971b16c01b5b5680e7
Pavel Bezdornov <pavel.alpinweb@yandex.ru>
2019-03-09 19:07:55 +0600

    create base for email sending

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const db = require("../models/db");
 const psw = require("../libs/password");
+const nodemailer = require("nodemailer");
+const config = require("../config/config.json");
 
 const isAdmin = (req, res, next) => {
   // если в сессии текущего пользователя есть пометка о том, что он является
@@ -29,14 +31,40 @@ router.post("/login/admin", (req, res) => {
     res.redirect("/login");
   }
 });
-
 router.post("/admin/skills", ctrlAdmin.skills);
-
 router.post("/admin/upload", ctrlAdmin.upload);
 
 router.get("/", ctrlHome.get);
-
 router.get("/login", ctrlLogin.get);
 router.get("/admin", isAdmin, ctrlAdmin.get);
+
+router.post("/", (req, res, next) => {
+  // требуем наличия имени, обратной почты и текста
+  if (!req.body.name || !req.body.email || !req.body.message) {
+    // если что-либо не указано - сообщаем об этом
+    return res.json({ msg: "Все поля нужно заполнить!", status: "Error" });
+  }
+  // инициализируем модуль для отправки писем и указываем данные из конфига
+  const transporter = nodemailer.createTransport(config.mail.smtp);
+  const mailOptions = {
+    from: `"${req.body.name}" <${req.body.email}>`,
+    to: config.mail.smtp.auth.user,
+    subject: config.mail.subject,
+    text:
+      req.body.message.trim().slice(0, 500) +
+      `\n Отправлено с: <${req.body.email}>`
+  };
+  // отправляем почту
+  transporter.sendMail(mailOptions, function(error, info) {
+    // если есть ошибки при отправке - сообщаем об этом
+    if (error) {
+      return res.json({
+        msg: `При отправке письма произошла ошибка!: ${error}`,
+        status: "Error"
+      });
+    }
+    res.json({ msg: "Письмо успешно отправлено!", status: "Ok" });
+  });
+});
 
 module.exports = router;
