6a161a176cb0783c237b2b0c2c06aac60f3572e7
Pavel Bezdornov <pavel.alpinweb@yandex.ru>
2019-03-09 13:29:34 +0600

    display skills from db

--- controllers/home.js
+++ controllers/home.js
@@ -1,5 +1,6 @@
 const db = require("../models/db");
 module.exports.get = function(req, res) {
   const works = db.getState().works || [];
-  res.render("pages/index", { products: works });
+  const skills = db.getState().skills || [];
+  res.render("pages/index", { products: works, skills: skills });
 };
