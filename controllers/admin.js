d2ac35581e55fc708ab1ffa6d7f1bd5b8ec2f810
Pavel Bezdornov <pavel.alpinweb@yandex.ru>
2019-03-09 18:25:54 +0600

    conect works to admin panel

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,18 +1,46 @@
 const db = require("../models/db");
+const fs = require("fs");
+const path = require("path");
+const formidable = require("formidable");
+
 module.exports.get = function(req, res) {
   res.render("pages/admin", { title: "admin" });
 };
 
 module.exports.upload = function(req, res, next) {
-  const { photo, name, price } = req.body;
-  db.get("works")
-    .push({
-      src: "./img/products/Work1.jpg",
-      name: name,
-      price: price
-    })
-    .write();
-  res.redirect("/#product");
+  // const { photo, name, price } = req.body;
+  let form = new formidable.IncomingForm();
+  let upload = path.join("./public", "upload");
+
+  if (!fs.existsSync(upload)) {
+    fs.mkdirSync(upload);
+  }
+
+  form.uploadDir = path.join(process.cwd(), upload);
+
+  form.parse(req, function(err, fields, files) {
+    if (err) {
+      return next(err);
+    }
+
+    const fileName = path.join(upload, files.photo.name);
+
+    fs.rename(files.photo.path, fileName, function(err) {
+      if (err) {
+        console.error(err.message);
+        return;
+      }
+
+      db.get("works")
+        .push({
+          src: `./upload/${files.photo.name}`,
+          name: fields.name,
+          price: fields.price
+        })
+        .write();
+      res.redirect("/#product");
+    });
+  });
 };
 
 module.exports.skills = function(req, res) {
