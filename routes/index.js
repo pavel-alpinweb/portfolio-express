54488146d0dd9477bf9502a2483de72a6f788123
pavel-alpinweb <pavel.alpinweb@yandex.ru>
2019-03-12 09:48:25 +0600

    replace mail function to home controler

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,6 @@ const express = require("express");
 const router = express.Router();
 const db = require("../models/db");
 const psw = require("../libs/password");
-const nodemailer = require("nodemailer");
-const config = require("../config.json");
-
-const works = db.getState().works || [];
-const skills = db.getState().skills || [];
 
 const ctrlHome = require("../controllers/home");
 const ctrlLogin = require("../controllers/login");
@@ -44,38 +39,6 @@ router.get("/", ctrlHome.get);
 router.get("/login", isAdmin, ctrlAdmin.get);
 router.get("/admin", isAdmin, ctrlAdmin.get);
 
-router.post("/", (req, res, next) => {
-  // требуем наличия имени, обратной почты и текста
-  if (!req.body.name || !req.body.email || !req.body.message) {
-    // если что-либо не указано - сообщаем об этом
-    return res.json({ msg: "Все поля нужно заполнить!", status: "Error" });
-  }
-  // инициализируем модуль для отправки писем и указываем данные из конфига
-  const transporter = nodemailer.createTransport(config.mail.smtp);
-  const mailOptions = {
-    from: `"${req.body.name}" <${req.body.email}>`,
-    to: config.mail.smtp.auth.user,
-    subject: config.mail.subject,
-    text:
-      req.body.message.trim().slice(0, 500) +
-      `\n Отправлено с: <${req.body.email}>`
-  };
-  // отправляем почту
-  transporter.sendMail(mailOptions, function(error, info) {
-    // если есть ошибки при отправке - сообщаем об этом
-    if (error) {
-      return res.render("pages/index", {
-        products: works,
-        skills: skills,
-        msg: `При отправке письма произошла ошибка!: ${error}`
-      });
-    }
-    res.render("pages/index", {
-      products: works,
-      skills: skills,
-      msg: `Письмо успешно отправлено: ${error}`
-    });
-  });
-});
+router.post("/", ctrlHome.postMail);
 
 module.exports = router;
